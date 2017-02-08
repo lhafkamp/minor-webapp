b827838f623e8c73f360a614de897cc653d91825
lhafkamp <Luuk@Luuks-MacBook-Pro.local>
2017-02-08 19:33:17 +0100

    added a watch on js with gulp

--- opdracht 5 - spa/gulpfile.js
+++ opdracht 5 - spa/gulpfile.js
@@ -2,7 +2,7 @@ var gulp        = require('gulp');
 var watch       = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['watch:css', 'browser-sync']);
+gulp.task('default', ['watch:js', 'watch:css', 'browser-sync']);
 
 gulp.task('watch:css', function() {
 	return gulp.watch('assets/css/styles.css', ['sync:css']);
@@ -10,6 +10,14 @@ gulp.task('watch:css', function() {
 
 gulp.task('sync:css', function() {
 	gulp.src('assets/css/styles.css').pipe(browserSync.stream());
+})
+
+gulp.task('watch:js', function() {
+	return gulp.watch('assets/js/app.js', ['sync:js']);
+});
+
+gulp.task('sync:js', function() {
+	gulp.src('assets/js/app.js').pipe(browserSync.stream());
 })
 
 // Static server
