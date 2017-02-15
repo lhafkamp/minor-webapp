e71df7b2ecae2d6b6f28073e6d902b4b37959884
lhafkamp <Luuk@141.204.028.145.hva.nl>
2017-02-15 11:54:01 +0100

    Started from scratch, new API (Github)

--- week 2/assignment 1 - data/assets/js/script.js
+++ week 2/assignment 1 - data/assets/js/script.js
@@ -1,26 +1,38 @@
-// first const data has the original data but can only be called 500 times a month, uncomment = great responsibility
-// const data = fetch('https://holidayapi.com/v1/holidays?key=a23b6d0b-186c-445d-b64c-4dec98245799&country=NL&year=2016');
+const mainContent = document.querySelector('#app');
+const userArray = []; // all the user objects
 
-function logData(data) {
-	renderToDom(Object.keys(data)
-		.map(hd => data[hd])
-		.map(hd => hd[0])
-		.map(hd => `<div>${hd.name}</div>`));
+function setUpDom(user) {
+	return `<div>${user.name} - ${user.bio}</div>`;
 }
 
-function renderToDom(divArray) {
-	document.querySelector('#app').innerHTML = divArray.reduce((html, div) => html += div);
+function renderToDom(elements) {
+	// TODO remove smt
+	const smt = mainContent.innerHTML = elements; // render ele's to HTML
+	console.log(smt);
+}
+
+function nameRender() {
+	const getUserInfo = userArray
+		.map(userInfo => setUpDom(userInfo)) // get html ele's
+		.join('');
+
+	renderToDom(getUserInfo);
+}
+
+function fetchSingleUser(user) {
+	const derp = fetch(user)
+		.then(data => data.json())
+		.then(data => userArray.push(data)) // push objects into userArray
+		.then(data => nameRender());
 }
 
 function fetchData() {
-	fetch('https://holidayapi.com/v1/holidays?key=3e22d5ed-edb5-4b5e-b0eb-72554b705c89&country=NL&year=2016')
-		.then(data => data.json())
-		.then((data) => {
-			logData(data.holidays);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+	const users = [
+	'https://api.github.com/users/lhafkamp', 
+	'https://api.github.com/users/nickrttn',
+	'https://api.github.com/users/rijkvanzanten',
+	];
+	users.forEach(user => fetchSingleUser(user));
 }
 
 fetchData();
